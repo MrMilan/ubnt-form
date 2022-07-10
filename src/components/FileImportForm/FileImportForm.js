3bdffadcc25a50f017d24de71d7d838e9e79f665
Milan Polacek <milan.polacek.getsjob@gmail.com>
2022-07-10 09:45:44 +0200

    Add validations to form

--- src/components/FileImportForm/FileImportForm.js
+++ src/components/FileImportForm/FileImportForm.js
@@ -1,7 +1,17 @@
 import React from "react"
 import { Field, reduxForm } from "redux-form"
 import CustomInput from "../CustomInput/CustomInput.js"
-import { required } from "./inputValidatorsService.js"
+import { 
+    required,
+    number,
+    minValue,
+    maxValue,
+    minLength,
+    maxLength,
+    isSelectedOneFile,
+    maxFileSize,
+ } from "./inputValidatorsService.js"
+
 
 const FileImportForm = () => {
     const handleSubmit = (values)=>{
@@ -10,23 +20,24 @@ const FileImportForm = () => {
       
     return (
         <div className="form-body">
-            <h2>Import file form</h2>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    {/* Name, min 1 char, max 100 char, required, text */}
-                    <Field name="name" type="text" component={CustomInput} label="Name" validate={required} required/>
-                </div>
-                <div>
-                    {/* Height, min 0 num, max 500 num, required, int  */}
-                    <Field name="height" type="number" component={CustomInput} label="Height" validate={required} required />
-                </div>
-                <div>
-                    {/* File, max 10MB*/}
-                    <label htmlFor="file">File</label>
-                    <Field name="file" type="file" component={CustomInput} />
-                </div>
-                <button type="submit">Submit</button>
-            </form>
+            <div className="form-body-card">
+                <h2>Import file form</h2>
+                <form onSubmit={handleSubmit}>
+                    <div>
+                        {/* Name, min 1 char, max 100 char, required, text */}
+                        <Field name="name" type="text" component={CustomInput} label="Name" validate={[required, minLength(1), maxLength(100)]} required />
+                    </div>
+                    <div>
+                        {/* Height, min 0 num, max 500 num, required, int  */}
+                        <Field name="height" type="number" component={CustomInput} label="Height" validate={[required, number, minValue(0), maxValue(500)]} required />
+                    </div>
+                    <div>
+                        {/* File, max 10MB*/}
+                        <Field name="file" type="file" component={CustomInput} label="File" validate={[isSelectedOneFile, maxFileSize(10)]} />
+                    </div>
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
         </div>
     )
 }
