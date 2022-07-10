24e211683e42a2d57866c6d46bc0ab006606127c
Milan Polacek <milan.polacek.getsjob@gmail.com>
2022-07-10 09:43:02 +0200

    Hide in Custom Input file input and replace with simple btn Add some css

--- src/components/CustomInput/CustomInput.js
+++ src/components/CustomInput/CustomInput.js
@@ -1,13 +1,37 @@
-import React from "react"
+import React, {useRef} from "react"
 import './CustomInput.css';
 
 
-const CustomInput = ({input, label, type, required, meta})=>(
-    <div className="custom-input">
-        <label className={`${required ? "input-required": "input-nonrequired"}`}>{label}</label>
-        <input {...input} type={type}/>
-        {meta.touched && meta.error && <span className="error-message">{meta.error}</span>}
-    </div>
-)
+const CustomInput = ({input, label, type, required, meta})=>{
+
+    const customInputRef = useRef();
+
+    const hancleClickToUpload = () => {
+        customInputRef.current.click();
+    };
+    
+    return (
+        <div className="custom-input">
+            <div className="custom-input-element">
+                <label className={`${required ? "input-required": "input-nonrequired"}`}>{label}</label>
+                <input 
+                    className={`${type==="file" ? "input-hidden": "input-visible"}`}
+                    {...input}
+                    type={type}
+                    ref={customInputRef}
+                    />
+                {   type==="file" &&
+                    <button className="custom-input-file-element" onClick={hancleClickToUpload}>
+                        Upload file
+                    </button>
+                }
+            </div>
+            <div className="custom-input-info">
+                {meta.touched && meta.error && <span className="error-message">{meta.error}</span>}
+                {meta.touched && meta.warning && <span className="error-warning">{meta.warning}</span>}
+            </div>
+        </div>
+    );
+}
 
 export default CustomInput;
